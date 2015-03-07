35f88d05c6abffe12a10678abe8a6fb08cc98086
Joel Tulloch <joel.tulloch@curvedental.com>
2015-03-07 08:37:45 -0800

    add parser for jerseys

---
+++ src/parsers/Jersey.js
@@ -0,0 +1,12 @@
+var parse = require('csv-parse');
+
+var columns = [ 'number', 'size' ];
+var parser_options = { 'columns': columns, 'ltrim': true, 'rtrim': true };
+
+module.exports = {
+    parse: function( input, callback ) {
+        parse( input, parser_options, function( err, output ) {
+            callback( output );
+        });
+    }
+};
---
+++ test/src/parsers/Jersey.js
@@ -0,0 +1,31 @@
+var assert = require("assert");
+var JerseyParser = require("../../../src/parsers/Jersey");
+
+describe('Jersey Parser', function() {
+    it('should return single jersey from single row', function( done ) {
+        var input = " 7, m"; // include intentional spaces
+        var expected_jerseys = [{ number: 7, size: 'm' }];
+
+        JerseyParser.parse(input,function( jerseys ) {
+            assert.deepEqual( jerseys, expected_jerseys );
+            done();
+        });
+    });
+
+    it('should return multiple jerseys from multiple rows', function( done ) {
+        var input = "7,m \n" +
+                    "3,l \n" +
+                    "20,s"
+
+        var expected_jerseys = [ 
+            { number: 7, size: 'm' },
+            { number: 3, size: 'l' },
+            { number: 20, size: 's' }
+        ];
+
+        JerseyParser.parse(input,function( jerseys ) {
+            assert.deepEqual( jerseys, expected_jerseys );
+            done();
+        });
+    });
+});
