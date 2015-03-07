a8101d55e77a354fe129adede3f90948ffa80f78
Joel Tulloch <joel.tulloch@curvedental.com>
2015-03-07 12:33:04 -0800

    fix test data filenames and add test for loader reset

--- test/src/FileLoader.js
+++ test/src/FileLoader.js
@@ -1,10 +1,12 @@
 var assert = require("assert");
 var FileLoader = require("../../src/FileLoader");
 var JerseyParser = require("../../src/parsers/Jersey");
+var TeamParser = require("../../src/parsers/Team");
+var Team = require("../../src/Team");
 
 describe('FileLoader', function() {
     it('should return content when no callback provided', function() {
-        var filename = 'test/data/one_jersey.js'; // requires tests to be run from project root
+        var filename = 'test/data/one_jersey.csv'; // requires tests to be run from project root
         var expected_output = '7,m\n';
 
         var output = FileLoader.load(filename);
@@ -13,7 +15,7 @@ describe('FileLoader', function() {
     });
 
     it('should use callback when provided', function(done) {
-        var filename = 'test/data/one_jersey.js'; // requires tests to be run from project root
+        var filename = 'test/data/one_jersey.csv'; // requires tests to be run from project root
         var expected_output = '7,m\n';
 
         FileLoader.load(filename,function(err,output) {
@@ -23,7 +25,7 @@ describe('FileLoader', function() {
     });
 
     it('should parse input when parser provided', function(done) {
-        var filename = 'test/data/one_jersey.js'; // requires tests to be run from project root
+        var filename = 'test/data/one_jersey.csv'; // requires tests to be run from project root
         var expected_output = [{number:7,size:'m'}];
 
         FileLoader
@@ -31,6 +33,37 @@ describe('FileLoader', function() {
             .load(filename,function(err,output) {
                 assert.deepEqual(output,expected_output);
                 done();
+            });
+    });
+
+    it('should parse different input when different parser provided', function(done) {
+        var jerseys_filename = 'test/data/one_jersey.csv'; // requires tests to be run from project root
+        var expected_jerseys = [{number:7,size:'m'}];
+
+        var teams_filename = 'test/data/one_team.csv'; // requires tests to be run from project root
+        var expected_teams = [new Team({ name: 'Geese', small_players: 3, medium_players: 5, large_players: 7 })];
+
+        var first_call = false;
+
+        FileLoader
+            .withParser(JerseyParser)
+            .load(jerseys_filename,function(err,output) {
+                assert.deepEqual(output,expected_jerseys);
+
+                if( first_call ) {
+                    done();
+                }
+                first_call = true;
+            });
+
+        FileLoader
+            .withParser(TeamParser)
+            .load(teams_filename,function(err,output) {
+                assert.deepEqual(output,expected_teams);
+                if( first_call ) {
+                    done();
+                }
+                first_call = true;
             });
     });
 });
