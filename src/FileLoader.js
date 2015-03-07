f55107271033ecfa1704dd46938b374fc5047d1c
Joel Tulloch <joel.tulloch@curvedental.com>
2015-03-07 12:07:38 -0800

    add support for parsers in FileLoader

--- src/FileLoader.js
+++ src/FileLoader.js
@@ -1,11 +1,31 @@
 var fs = require('fs');
-
 var options = { encoding: 'utf8' };
 
-module.exports = {
+var parser = null;
+
+var wrapCallback = function(callback,_parser) {
+    if( !_parser ) {
+        return callback;
+    }
+
+    return function(err,data) {
+        _parser.parse(data,callback);
+    };
+};
+
+var reset = function() {
+    parser = null;
+};
+
+module.exports = loader = {
+    withParser: function(_parser) {
+        parser = _parser;
+        return loader;
+    },
     load: function(file_name,callback) {
         if(callback) {
-            fs.readFile(file_name, options, callback)
+            fs.readFile(file_name, options, wrapCallback(callback,parser))
+            reset();
             return;
         }
         return fs.readFileSync(file_name, options);
--- test/src/FileLoader.js
+++ test/src/FileLoader.js
@@ -1,5 +1,6 @@
 var assert = require("assert");
 var FileLoader = require("../../src/FileLoader");
+var JerseyParser = require("../../src/parsers/Jersey");
 
 describe('FileLoader', function() {
     it('should return content when no callback provided', function() {
@@ -19,5 +20,17 @@ describe('FileLoader', function() {
             assert.equal(output,expected_output);
             done();
         });
+    });
+
+    it('should parse input when parser provided', function(done) {
+        var filename = 'test/data/one_jersey.js'; // requires tests to be run from project root
+        var expected_output = [{number:7,size:'m'}];
+
+        FileLoader
+            .withParser(JerseyParser)
+            .load(filename,function(err,output) {
+                assert.deepEqual(output,expected_output);
+                done();
+            });
     });
 });
