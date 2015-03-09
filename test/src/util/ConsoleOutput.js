e3680d95d59f7f330010c743412dd5d57fdde1b5
Joel Tulloch <joel.tulloch@curvedental.com>
2015-03-09 08:53:08 -0700

    suppress console.log output during test

--- test/src/util/ConsoleOutput.js
+++ test/src/util/ConsoleOutput.js
@@ -3,7 +3,20 @@ var simple = require("simple-mock");
 
 var ConsoleOutput = require('../../../src/util/ConsoleOutput');
 
+var logger;
+function suppressLog() {
+    logger = console.log;
+    console.log = function(){};
+}
+
+function restoreLog() {
+    console.log = logger;
+}
+
 describe('ConsoleOutput', function() {
+    before( suppressLog );
+    after( restoreLog );
+
     it('should call formatter once', function() {
         var test_team = {name: 'Lizards'};
         var spy = simple.stub();
